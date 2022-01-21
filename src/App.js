e98276f5f7894a8ca671143bcbe51e8103e04ea6
himaranimathews <himaranimathews@gmail.com>
2022-01-21 09:47:48 +0530

    Added Files

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="output">
+        <div className="previousOutput">123*</div>
+        <div clasName="nextOutput">123</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button className="">DEL</button>
+      <button className="">÷</button>
+      <button className="">7</button>
+      <button className="">8</button>
+      <button className="">9</button>
+      <button className="">*</button>
+      <button className="">4</button>
+      <button className="">5</button>
+      <button className="">6</button>
+      <button className="">-</button>
+      <button className="">1</button>
+      <button className="">2</button>
+      <button className="">3</button>
+      <button className="">+</button>
+      <button className="">.</button>
+      <button className="">0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+};
+
+export default App;
