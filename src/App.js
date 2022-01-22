e07fb15b4adf99ffd6cd5bdafe3012caa213a9f1
himaranimathews <himaranimathews@gmail.com>
2022-01-22 13:19:19 +0530

    Added Files

--- src/App.js
+++ src/App.js
@@ -1,32 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 const App = () => {
+  const [expression, setExpression] = useState("");
+  const display = (symbol) => {
+    setExpression((prev) => prev + symbol);
+  };
+
+  const [answer, setAnswer] = useState(0);
+  const calculate = () => {
+    setAnswer(eval(expression));
+  };
+
+  const clear = () => {
+    setAnswer((prev) =>
+      prev
+        .split("")
+        .slice(0, prev.length - 1)
+        .join("")
+    );
+    setAnswer(0);
+  };
+  const allClear = () => {
+    setExpression("0");
+    setAnswer("0");
+  };
+
   return (
     <div className="container">
       <div className="grid">
-      <div className="output">
-        <div className="previousOutput">123*</div>
-        <div clasName="nextOutput">123</div>
+        <div className="output">
+          <div className="previousOutput" >{expression}</div>
+          <div className="nextOutput" >{answer}</div>
+        </div>
+        <button onClick={allClear} className="inner-grid AC">
+          AC
+        </button>
+        <button onClick={clear} className="inner-grid ">
+          DEL
+        </button>
+        <button onClick={() => display("/")} className="inner-grid ">
+          ÷
+        </button>
+        <button onClick={() => display("7")} className="inner-grid ">
+          7
+        </button>
+        <button onClick={() => display("8")} className="inner-grid ">
+          8
+        </button>
+        <button onClick={() => display("9")} className="inner-grid ">
+          9
+        </button>
+        <button onClick={() => display("*")} className="inner-grid ">
+          *
+        </button>
+        <button onClick={() => display("4")} className="inner-grid ">
+          4
+        </button>
+        <button onClick={() => display("5")} className="inner-grid ">
+          5
+        </button>
+        <button onClick={() => display("6")} className="inner-grid ">
+          6
+        </button>
+        <button onClick={() => display("-")} className="inner-grid ">
+          -
+        </button>
+        <button onClick={() => display("1")} className="inner-grid ">
+          1
+        </button>
+        <button onClick={() => display("2")} className="inner-grid ">
+          2
+        </button>
+        <button onClick={() => display("3")} className="inner-grid ">
+          3
+        </button>
+        <button onClick={() => display("+")} className="inner-grid ">
+          +
+        </button>
+        <button onClick={() => display(".")} className="inner-grid ">
+          .
+        </button>
+        <button onClick={() => display("0")} className="inner-grid ">
+          0
+        </button>
+        <button onClick={calculate} className="inner-grid equal">
+          =
+        </button>
       </div>
-      <button className="span-two">AC</button>
-      <button className="">DEL</button>
-      <button className="">÷</button>
-      <button className="">7</button>
-      <button className="">8</button>
-      <button className="">9</button>
-      <button className="">*</button>
-      <button className="">4</button>
-      <button className="">5</button>
-      <button className="">6</button>
-      <button className="">-</button>
-      <button className="">1</button>
-      <button className="">2</button>
-      <button className="">3</button>
-      <button className="">+</button>
-      <button className="">.</button>
-      <button className="">0</button>
-      <button className="span-two">=</button>
-    </div>
     </div>
   );
 };
